ae938a8b645bef989a06e78ef3a32144cf9517c3
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-10-14 08:28:32 +0600

    video like dislike serializers created

---
+++ videos/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Video, Like, Dislike
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = [
+            'id',
+            'title',
+            'slug',
+            'description',
+            'youtube_video_id',
+            'author_id',
+            'like_list',
+            'dislike_list',
+            'created_at'
+        ]
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['id', 'video', 'user_id', 'created_at']
+
+
+class DisLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dislike
+        fields = ['id', 'video', 'user_id', 'created_at']
